6c6dba3b1bc5c34266057237c464136841d14059
nutsand <nmimosan@gmail.com>
2024-02-12 22:33:05 +0900

    add iam setting for cicd pipeline

--- envs/prod/cicd/app_foobar/iam.tf
+++ envs/prod/cicd/app_foobar/iam.tf
@@ -1,7 +1,45 @@
+# GitHub Action がアクセスキーでアクセスするユーザ
 resource "aws_iam_user" "github" {
   name = "${local.name_prefix}-${local.service_name}-github"
 
   tags = {
     Name = "${local.name_prefix}-${local.service_name}-github"
   }
+}
+
+# GitHub Action用にAssumeRoleするIAMロール
+resource "aws_iam_role" "deployer" {
+  name = "${local.name_prefix}-${local.service_name}-deployer"
+
+  assume_role_policy = jsonencode(
+    {
+        "Version" : "2012-10-17",
+        "Statement" : [
+            {
+                "Effect" : "Allow",
+                "Action": [
+                    "sts:AssumeRole",
+                    "sts:TagSession"
+                ],
+                "Principal": {
+                    "AWS": aws_iam_user.github.arn
+                }
+            }
+        ]
+    }
+  )
+
+  tags = {
+    Name = "${local.name_prefix}-${local.service_name}-deployer"
+  }
+}
+
+# dataは外部のリソースにアクセス（dataでAWSのデフォルトのpolicyを指定）
+data "aws_iam_policy" "ecr_power_user" {
+  arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role_policy_attachment" "role_deployer_policy_ecr_power_user" {
+  role = aws_iam_role.deployer.name
+  policy_arn = data.aws_iam_policy.ecr_power_user.arn
 }
